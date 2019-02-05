a8a06e8c70315724ea830f65de1839952eb1f7c9
GreenArrowRepo <hims.mehuriya@gmail.com>
2019-02-06 02:35:01 +0530

    Created rotating astroid L2-09
    Creating hazards

---
+++ Space Shooter Unity 2018.2.10f1/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collided");
+        if (other.tag == "Boundry")
+        {
+            Debug.Log("collided with boundary");
+            return;
+        }
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+
+}
---
+++ Space Shooter Unity 2018.2.10f1/Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class RandomRotator : MonoBehaviour
+{
+    [SerializeField]
+    private float tumble;
+
+    private new Rigidbody rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        rigidbody.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+
+
+}
