5b3531899724806610c76757b75d205e6e4f4411
GreenArrowRepo <hims.mehuriya@gmail.com>
2019-02-05 22:26:27 +0530

    Added Bolt bullet VFX
    Added Bolt and created scirpt to move bullet

---
+++ Space Shooter Unity 2018.2.10f1/Assets/Scripts/Mover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    private new Rigidbody rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody.velocity = transform.forward * speed;
+    }
+    
+}
