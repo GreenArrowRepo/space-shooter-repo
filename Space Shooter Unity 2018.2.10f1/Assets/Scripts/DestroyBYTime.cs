498c2203ecc1dd3461bd4e89f565d02b27d0cacf
GreenArrowRepo <hims.mehuriya@gmail.com>
2019-02-07 00:39:38 +0530

    Spawning waves L2-12
    Added code to spawn waves in GameController script

---
+++ Space Shooter Unity 2018.2.10f1/Assets/Scripts/DestroyBYTime.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBYTime : MonoBehaviour {
+    [SerializeField]
+    private float lifetime;
+
+    void Start ()
+    {
+        Destroy(gameObject, lifetime); // the particle will be destroyed after specified lifetime after it is played.
+	}
+	
+	
+}
--- Space Shooter Unity 2018.2.10f1/Assets/Scripts/GameController.cs
+++ Space Shooter Unity 2018.2.10f1/Assets/Scripts/GameController.cs
@@ -7,16 +7,32 @@ public class GameController : MonoBehaviour
     private GameObject hazard;
     [SerializeField]
     private Vector3 spawnValues;
-    
+    [SerializeField]
+    private int hazardCount;
+    [SerializeField]
+    private float startWait = 1f;
+    [SerializeField]
+    private float spawnWait = 0.5f;
+    private float waveWait = 4f;
+
     private void Start()
     {
-        spawnWaves();
+        StartCoroutine(spawnWaves());
     }
 
-    private void spawnWaves()
+    private IEnumerator spawnWaves()
     {
-        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x),spawnValues.y, spawnValues.z);
-        Quaternion spawnRotation = Quaternion.identity;
-        Instantiate(hazard, spawnPosition, spawnRotation);
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
     }
 }
