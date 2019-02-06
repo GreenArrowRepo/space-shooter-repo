ddc98b196816de0daff3526af59de99ecefb67a5
GreenArrowRepo <hims.mehuriya@gmail.com>
2019-02-06 21:25:17 +0530

    Game controller L2-11
    Added game controller to instantiate astroid from the top of the screen from ramdom positions.

---
+++ Space Shooter Unity 2018.2.10f1/Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject hazard;
+    [SerializeField]
+    private Vector3 spawnValues;
+    
+    private void Start()
+    {
+        spawnWaves();
+    }
+
+    private void spawnWaves()
+    {
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x),spawnValues.y, spawnValues.z);
+        Quaternion spawnRotation = Quaternion.identity;
+        Instantiate(hazard, spawnPosition, spawnRotation);
+    }
+}
